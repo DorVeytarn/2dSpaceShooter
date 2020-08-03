0f88eeefed5d1c12c32d88a05bf2089dc0ae3456
DorVeytarn <63097757+DorVeytarn@users.noreply.github.com>
2020-08-03 23:38:06 +0300

    1. Сделал скрипт экрана проигрышка. Он вылазит при смерти) 2. Добавил его на экран

---
+++ Assets/Scripts/UI/GameOverScreen.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class GameOverScreen : MonoBehaviour
+{
+    [SerializeField] private Button _exitButton;
+    [SerializeField] private Button _restartButton;
+    [SerializeField] private Player _player;
+
+    private CanvasGroup _gameOverGroup;
+
+    private void OnEnable()
+    {
+        _player.Died += OnDied;
+        _exitButton.onClick.AddListener(OnExitButtonClick);
+        _restartButton.onClick.AddListener(OnRestartButtonClick);
+    }
+
+    private void OnDisable()
+    {
+        _player.Died -= OnDied;
+        _exitButton.onClick.RemoveListener(OnExitButtonClick);
+        _restartButton.onClick.RemoveListener(OnRestartButtonClick);
+    }
+
+    private void Start()
+    {
+        _gameOverGroup = GetComponent<CanvasGroup>();
+        _gameOverGroup.alpha = 0;
+    }
+
+    private void OnDied()
+    {
+        Time.timeScale = 0;
+        _gameOverGroup.alpha = 1;
+    }
+
+    private void OnRestartButtonClick()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
+    }
+
+    private void OnExitButtonClick()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
